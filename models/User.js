e16a55f78f0d5041d976a61a3f738cdc3e0714e8
Ronan-Codes <galv.ronan@gmail.com>
2021-07-11 16:56:50 -0400

    starter files and started on models

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { schema, model, Schema, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique:true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+\@.+\..+/, 'Please enter a valid email address!']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                // Schema.Types.ObjectId,
+                type: String,
+                ref: 'User'   
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval 
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema)
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const { Server } = require('http');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+// Create or connect db
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/the-social-network-api', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
